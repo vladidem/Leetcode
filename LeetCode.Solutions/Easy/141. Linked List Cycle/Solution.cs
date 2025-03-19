17809bbd8186efa70e963e3c0fbb2e987bb6eeea
lairotele <lairotele@gmail.com>
2025-03-19 19:52:40 +0300

    Add solution for 141. Linked List Cycle

---
+++ LeetCode.Solutions/Easy/141. Linked List Cycle/Solution.cs
@@ -0,0 +1,23 @@
+﻿using LeetCode.Solutions.DataStructures;
+
+namespace LeetCode.Solutions.Easy._1141_Linked_List_Cycle;
+
+/// <summary>
+///     https://leetcode.com/problems/linked-list-cycle/
+/// </summary>
+public class Solution
+{
+    public bool HasCycle(ListNode head)
+    {
+        var slow = head;
+        var fast = head?.next;
+
+        while (fast != slow && fast?.next?.next != null)
+        {
+            slow = slow?.next;
+            fast = fast?.next?.next;
+        }
+
+        return fast != null && fast == slow;
+    }
+}
---
+++ LeetCode.Solutions/Easy/141. Linked List Cycle/Tests.cs
@@ -0,0 +1,50 @@
+using FluentAssertions;
+using LeetCode.Solutions.DataStructures;
+
+namespace LeetCode.Solutions.Easy._1141_Linked_List_Cycle;
+
+public class Tests
+{
+    [TestCase(new[] { 2, 7, 11, 15 }, 3)]
+    [TestCase(new[] { 3, 4, 2 }, 2)]
+    [TestCase(new[] { 3, 3 }, 1)]
+    public void HasCycle_ListWithNotFullCycle_ReturnsTrue(int[] nums, int cyclePosition)
+    {
+        var head = ListNode.FromArray(nums);
+        var cycle = head.Nth(cyclePosition);
+        var last = head.Last();
+
+        last.next = cycle;
+
+        new Solution().HasCycle(head).Should().BeTrue();
+    }
+
+    [TestCase(new[] { 2, 7, 11, 15 })]
+    [TestCase(new[] { 3, 4, 2 })]
+    [TestCase(new[] { 3, 3 })]
+    public void HasCycle_ListWithFullCycle_ReturnsTrue(int[] nums)
+    {
+        var head = ListNode.FromArray(nums);
+        var last = head.Last();
+
+        last.next = head;
+
+        new Solution().HasCycle(head).Should().BeTrue();
+    }
+
+    [TestCase(new[] { 2, 7, 11, 15 })]
+    [TestCase(new[] { 3, 4, 2 })]
+    [TestCase(new[] { 3, 3 })]
+    public void HasCycle_ListWithoutCycle_ReturnsFalse(int[] nums)
+    {
+        var head = ListNode.FromArray(nums);
+
+        new Solution().HasCycle(head).Should().BeFalse();
+    }
+
+    [Test]
+    public void HasCycle_EmptyList_ReturnsFalse()
+    {
+        new Solution().HasCycle(null).Should().BeFalse();
+    }
+}
