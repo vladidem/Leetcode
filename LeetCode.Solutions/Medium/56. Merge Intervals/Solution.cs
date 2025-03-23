5c579b10c327c8010cd7055170fdeae150a7def3
Vladimir Demkin <lairotele@gmail.com>
2025-03-23 13:59:21 +0300

    Add solution for 56. Merge Intervals

---
+++ LeetCode.Solutions/Medium/56. Merge Intervals/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Solutions.Medium._56_Merge_Intervals;
+
+/// <summary>
+///     https://leetcode.com/problems/merge-intervals/
+/// </summary>
+public class Solution
+{
+    public int[][] Merge(int[][] intervals)
+    {
+        if (intervals.Length == 1)
+            return intervals;
+
+        var sortedIntervals = intervals.OrderBy(i => i[0]).ToArray();
+        var start = sortedIntervals[0][0];
+        var end = sortedIntervals[0][1];
+        var result = new List<int[]>();
+
+        foreach (var interval in sortedIntervals)
+            if (interval[0] <= end)
+            {
+                end = Math.Max(interval[1], end);
+            }
+            else
+            {
+                result.Add([start, end]);
+                start = interval[0];
+                end = interval[1];
+            }
+
+        result.Add([start, end]);
+
+        return result.ToArray();
+    }
+}
---
+++ LeetCode.Solutions/Medium/56. Merge Intervals/Tests.cs
@@ -0,0 +1,28 @@
+using FluentAssertions;
+
+namespace LeetCode.Solutions.Medium._56_Merge_Intervals;
+
+public class Tests
+{
+    public static TestData[] MergeIntervals_IntervalsIntersect_ReturnsMergedCases =
+    {
+        new(
+            [[6, 7], [2, 4], [5, 9]],
+            [[2, 4], [5, 9]]
+        ),
+        new(
+            [[1, 4], [2, 6], [3, 5]],
+            [[1, 6]]
+        )
+    };
+
+    [TestCaseSource(nameof(MergeIntervals_IntervalsIntersect_ReturnsMergedCases))]
+    public void MergeIntervals_IntervalsIntersect_ReturnsMerged(TestData testData)
+    {
+        var result = new Solution().Merge(testData.Intervals);
+
+        result.Should().BeEquivalentTo(testData.Expected);
+    }
+
+    public record TestData(int[][] Intervals, int[][] Expected);
+}
