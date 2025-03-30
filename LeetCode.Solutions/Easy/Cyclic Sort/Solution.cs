8a3a3780a458f1ca6ab522956cc1a233a00f9bdc
Vladimir Demkin <lairotele@gmail.com>
2025-03-30 16:27:47 +0300

    Fix typo, remove variable

--- LeetCode.Solutions/Easy/Cyclic Sort/Solution.cs
+++ LeetCode.Solutions/Easy/Cyclic Sort/Solution.cs
@@ -5,10 +5,7 @@ public class Solution
     public void Sort(int[] nums)
     {
         for (var i = 0; i < nums.Length - 1; i++)
-        {
-            var j = i;
-            while (nums[j] != j - 1)
-                (nums[j], nums[nums[j] - 1]) = (nums[nums[j] - 1], nums[j]);
-        }
+            while (nums[i] != i + 1)
+                (nums[i], nums[nums[i] - 1]) = (nums[nums[i] - 1], nums[i]);
     }
 }
