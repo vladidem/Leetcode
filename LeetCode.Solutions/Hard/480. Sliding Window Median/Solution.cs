f5f475bcc4c6bbe6c76a9c69d5f697ebfdece08c
Vladimir Demkin <lairotele@gmail.com>
2025-04-02 14:45:49 +0300

    Add solution for 480. Sliding Window Median

---
+++ LeetCode.Solutions/Hard/480. Sliding Window Median/Solution.cs
@@ -0,0 +1,91 @@
+﻿namespace LeetCode.Solutions.Hard._480_Sliding_Window_Median;
+
+/// <summary>
+///     https://leetcode.com/problems/sliding-window-median/
+/// </summary>
+public class Solution
+{
+    public double[] MedianSlidingWindow(int[] nums, int k)
+    {
+        if (k == 1) return nums.Select(it => (double)it).ToArray();
+
+        var medianFinder = new MedianFinder();
+        var result = new List<double>();
+        for (var i = 0; i < nums.Length; i++)
+        {
+            medianFinder.AddNum(nums[i]);
+            if (i < k - 1)
+                continue;
+
+            if (i - k >= 0)
+                medianFinder.RemoveNum(nums[i - k]);
+            result.Add(medianFinder.FindMedian());
+        }
+
+        return result.ToArray();
+    }
+
+    public class MedianFinder
+    {
+        public MedianFinder()
+        {
+            // Descending Sort
+            _firstHalf =
+                new PriorityQueue<int, int>(Comparer<int>.Create((x, y) => -Comparer<int>.Default.Compare(x, y)));
+
+            // Ascending Sort
+            _secondHalf =
+                new PriorityQueue<int, int>(Comparer<int>.Create((x, y) => Comparer<int>.Default.Compare(x, y)));
+        }
+
+        private PriorityQueue<int, int> _firstHalf { get; }
+        private PriorityQueue<int, int> _secondHalf { get; }
+
+        public void AddNum(int num)
+        {
+            _firstHalf.Enqueue(num, num);
+
+            BalanceQueues();
+        }
+
+        public void RemoveNum(int num)
+        {
+            if (num < _secondHalf.Peek())
+                _firstHalf.Remove(num, out _, out _);
+            else
+                _secondHalf.Remove(num, out _, out _);
+            BalanceQueues();
+        }
+
+        private void BalanceQueues()
+        {
+            if (_firstHalf.Count > _secondHalf.Count)
+            {
+                var element = _firstHalf.Dequeue();
+                _secondHalf.Enqueue(element, element);
+            }
+            else if (_firstHalf.Count == _secondHalf.Count && _firstHalf.Peek() > _secondHalf.Peek())
+            {
+                var firstElement = _firstHalf.Dequeue();
+                _secondHalf.Enqueue(firstElement, firstElement);
+
+                var secondElement = _secondHalf.Dequeue();
+                _firstHalf.Enqueue(secondElement, secondElement);
+            }
+
+            while (_secondHalf.Count - _firstHalf.Count > 1)
+            {
+                var element = _secondHalf.Dequeue();
+                _firstHalf.Enqueue(element, element);
+            }
+        }
+
+        public double FindMedian()
+        {
+            if (_firstHalf.Count == _secondHalf.Count)
+                return (double)_firstHalf.Peek() / 2 + (double)_secondHalf.Peek() / 2;
+
+            return _secondHalf.Peek();
+        }
+    }
+}
---
+++ LeetCode.Solutions/Hard/480. Sliding Window Median/Tests.cs
@@ -0,0 +1,34 @@
+using FluentAssertions;
+
+namespace LeetCode.Solutions.Hard._480_Sliding_Window_Median;
+
+public class Tests
+{
+    [TestCase(3, new[] { 1, 3, -1, -3, 5, 3, 6, 7 }, new[] { 1.00000, -1.00000, -1.00000, 3.00000, 5.00000, 6.00000 })]
+    [TestCase(3, new[] { 1, 2, 3, 4, 2, 3, 1, 4, 2 },
+        new[] { 2.00000, 3.00000, 3.00000, 3.00000, 2.00000, 3.00000, 2.00000 })]
+    public void MedianSlidingWindow_NotEmptySequence_ReturnsMediansForWindows(int k, int[] numbers, double[] result)
+    {
+        var solution = new Solution().MedianSlidingWindow(numbers, k);
+        solution.Should().BeEquivalentTo(result, opt => opt.WithStrictOrdering());
+    }
+
+    [TestCase(2, new[] { 2147483647, 1, 2, 3, 4, 5, 6, 7, 2147483647 },
+        new[] { 1073741824.00000, 1.50000, 2.50000, 3.50000, 4.50000, 5.50000, 6.50000, 1073741827.00000 })]
+    [TestCase(3,
+        new[]
+        {
+            -2147483648, -2147483648, 2147483647, -2147483648, -2147483648, -2147483648, 2147483647, 2147483647,
+            2147483647, 2147483647, -2147483648, 2147483647, -2147483648
+        },
+        new[]
+        {
+            -2147483648.00000, -2147483648.00000, -2147483648.00000, -2147483648.00000, -2147483648.00000,
+            2147483647.00000, 2147483647.00000, 2147483647.00000, 2147483647.00000, 2147483647.00000, -2147483648.00000
+        })]
+    public void MedianSlidingWindow_Overflow_ReturnsMediansForWindows(int k, int[] numbers, double[] result)
+    {
+        var solution = new Solution().MedianSlidingWindow(numbers, k);
+        solution.Should().BeEquivalentTo(result, opt => opt.WithStrictOrdering());
+    }
+}
