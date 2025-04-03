3faf363921c58e9a056418a389c0491eee8cf705
Vladimir Demkin <lairotele@gmail.com>
2025-04-03 19:56:38 +0300

    Add solution for 46. Permutations

---
+++ LeetCode.Solutions/Medium/46. Permutations/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Immutable;
+
+namespace LeetCode.Solutions.Medium._46_Permutations;
+
+/// <summary>
+///     https://leetcode.com/problems/permutations/
+/// </summary>
+public class Solution
+{
+    public IList<IList<int>> Permute(int[] nums)
+    {
+        return PermuteIterative(nums);
+    }
+
+    public IList<IList<int>> PermuteIterative(int[] nums)
+    {
+        if (nums.Length == 0)
+            return [];
+        IList<IList<int>> mutations = new List<IList<int>>();
+        mutations.Add([nums[0]]);
+
+        for (var i = 1; i < nums.Length; i++)
+            mutations = mutations.SelectMany(mutation => NewMutations(nums, nums[i], mutation)).ToList();
+
+        return mutations;
+    }
+
+    private IEnumerable<IList<int>> NewMutations(int[] nums, int newNum, IList<int> mutation)
+    {
+        for (var j = 0; j <= mutation.Count; j++)
+        {
+            IList<int> newMutation = new List<int>(mutation);
+            newMutation.Insert(j, newNum);
+            yield return newMutation;
+        }
+    }
+
+    public IEnumerable<IList<int>> PermuteRecursive(int[] nums, ImmutableHashSet<int> visited, IList<int> path)
+    {
+        if (path.Count == nums.Length) return [path];
+        return nums.Where(it => !visited.Contains(it)).SelectMany(it =>
+        {
+            IList<int> newPath = new List<int>(path);
+            newPath.Add(it);
+            return PermuteRecursive(nums, visited.Add(it), newPath);
+        });
+    }
+}
---
+++ LeetCode.Solutions/Medium/46. Permutations/Tests.cs
@@ -0,0 +1,32 @@
+using FluentAssertions;
+
+namespace LeetCode.Solutions.Medium._46_Permutations;
+
+public class Tests
+{
+    public static TestData[] Permute_NotEmpty_ReturnsAllPermutationsCases =
+    {
+        new(
+            [1, 2, 3],
+            [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+        ),
+        new(
+            [0, 1],
+            [[0, 1], [1, 0]]
+        ),
+        new(
+            [1],
+            [[1]]
+        )
+    };
+
+    [TestCaseSource(nameof(Permute_NotEmpty_ReturnsAllPermutationsCases))]
+    public void Permute_NotEmpty_ReturnsAllPermutations(TestData testData)
+    {
+        var result = new Solution().Permute(testData.Nums);
+
+        result.Should().BeEquivalentTo(testData.Expected);
+    }
+
+    public record TestData(int[] Nums, int[][] Expected);
+}
