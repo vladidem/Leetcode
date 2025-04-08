6a014f55d2eb01035a34a233f56bed8be510d602
Vladimir Demkin <lairotele@gmail.com>
2025-04-08 18:11:51 +0300

    Add solution for 658. Find K Closest Elements

---
+++ LeetCode.Solutions/Medium/658. Find K Closest Elements/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Solutions.Medium._658_Find_K_Closest_Elements;
+
+/// <summary>
+///     https://leetcode.com/problems/find-k-closest-elements/
+/// </summary>
+public class Solution
+{
+    public IList<int> FindClosestElements(int[] arr, int k, int x)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (x < arr[mid])
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+
+        int rangeStart = Math.Max(0, left - k);
+        int rangeEnd = Math.Min(arr.Length - 1, left + k);
+        while (rangeEnd - rangeStart < k)
+        {
+            if (ACloserThanB(x, arr[rangeStart], arr[rangeEnd]))
+                rangeEnd++;
+            else
+                rangeStart--;
+        }
+
+        return arr[rangeStart..(rangeEnd + 1)];
+    }
+
+    private bool ACloserThanB(int x, int a, int b)
+    {
+        return Math.Abs(x - a) < Math.Abs(x - b)
+               || (Math.Abs(x - a) == Math.Abs(x - b) && a < b);
+    }
+}
---
+++ LeetCode.Solutions/Medium/658. Find K Closest Elements/Tests.cs
@@ -0,0 +1,51 @@
+using FluentAssertions;
+
+namespace LeetCode.Solutions.Medium._658_Find_K_Closest_Elements;
+
+public class Tests
+{
+    public static TestData[] FindClosestElements_TargetExitst_ReturnsRangeCases =
+    {
+        new(
+            [1, 2, 3, 4, 5],
+            4,
+            3,
+            [1, 2, 3, 4]
+        ),
+        new(
+            [1, 1, 2, 3, 4, 5],
+            4,
+            -1,
+            [1, 1, 2, 3]
+        )
+    };
+
+
+    public static TestData[] FindClosestElements_EdgeCases_ReturnsRangeCases =
+    {
+        new(
+            [1],
+            1,
+            1,
+            [1]
+        )
+    };
+
+    [TestCaseSource(nameof(FindClosestElements_TargetExitst_ReturnsRangeCases))]
+    public void FindClosestElements_TargetExitst_ReturnsRange(TestData testData)
+    {
+        var result = new Solution().FindClosestElements(testData.Nums, testData.K, testData.X);
+
+        result.Should().BeEquivalentTo(testData.Expected);
+    }
+
+    [TestCaseSource(nameof(FindClosestElements_EdgeCases_ReturnsRangeCases))]
+    public void FindClosestElements_EdgeCases_ReturnsRange(TestData testData)
+    {
+        var result = new Solution().FindClosestElements(testData.Nums, testData.K, testData.X);
+
+        result.Should().BeEquivalentTo(testData.Expected);
+    }
+
+    public record TestData(int[] Nums, int K, int X, int[] Expected);
+}
