eb9d9909fb31712967c0e5831ae839b291d937c5
Vladimir Demkin <lairotele@gmail.com>
2025-04-09 14:22:29 +0300

    Add solution for 136. Single Number

---
+++ LeetCode.Solutions/Easy/136. Single Number/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Solutions.Easy._136_Single_Number;
+
+/// <summary>
+///     https://leetcode.com/problems/single-number/
+/// </summary>
+public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        int result = 0;
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            result ^= nums[i];
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode.Solutions/Easy/136. Single Number/Tests.cs
@@ -0,0 +1,12 @@
+namespace LeetCode.Solutions.Easy._136_Single_Number;
+
+public class Tests
+{
+    [TestCase(new[] { 2, 2, 1 }, ExpectedResult = 1)]
+    [TestCase(new[] { 4, 1, 2, 1, 2 }, ExpectedResult = 4)]
+    [TestCase(new[] { 1 }, ExpectedResult = 1)]
+    public int SingleNumber_ResultExist_ReturnsSingleNumber(int[] nums)
+    {
+        return new Solution().SingleNumber(nums);
+    }
+}
