9f281f7468b967e4f2fe70de62944ac99b6d426d
Vladimir Demkin <lairotele@gmail.com>
2025-04-15 13:45:05 +0300

    Add solution for 632. Smallest Range Covering Elements from K Lists

---
+++ LeetCode.Solutions/Hard/632. Smallest Range Covering Elements from K Lists/Solution.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode.Solutions.Hard._632_Smallest_Range_Covering_Elements_from_K_Lists;
+
+/// <summary>
+///     https://leetcode.com/problems/smallest-range-covering-elements-from-k-lists/
+/// </summary>
+public class Solution
+{
+    public int[] SmallestRange(IList<IList<int>> nums)
+    {
+        var minHeap = new PriorityQueue<IEnumerator<int>, int>(Comparer<int>.Create((x, y) => x.CompareTo(y)));
+        int bottom = int.MaxValue;
+        int top = int.MinValue;
+
+        foreach (var row in nums)
+        {
+            var rowEnumerator = row.AsEnumerable().GetEnumerator();
+            if (rowEnumerator.MoveNext())
+            {
+                if (rowEnumerator.Current < bottom)
+                    bottom = rowEnumerator.Current;
+                if (rowEnumerator.Current > top)
+                    top = rowEnumerator.Current;
+
+                minHeap.Enqueue(rowEnumerator, rowEnumerator.Current);
+            }
+        }
+
+        int[] minRange = [bottom, top];
+        while (minHeap.Count > 0)
+        {
+            var enumerator = minHeap.Dequeue();
+            if (!enumerator.MoveNext())
+                break;
+
+            if (enumerator.Current > top)
+                top = enumerator.Current;
+            minHeap.Enqueue(enumerator, enumerator.Current);
+            bottom = minHeap.Peek().Current;
+            if (top - bottom < minRange[1] - minRange[0])
+            {
+                minRange = [bottom, top];
+            }
+        }
+
+        return minRange;
+    }
+}
---
+++ LeetCode.Solutions/Hard/632. Smallest Range Covering Elements from K Lists/Tests.cs
@@ -0,0 +1,29 @@
+using FluentAssertions;
+
+namespace LeetCode.Solutions.Hard._632_Smallest_Range_Covering_Elements_from_K_Lists;
+
+public class Tests
+{
+    public static TestData[] SmallestRange_SortedLists_ReturnsMinRangeCases =
+    {
+        new(
+            [[4, 10, 15, 24, 26], [0, 9, 12, 20], [5, 18, 22, 30]],
+            [20, 24]
+        ),
+        new(
+            [[1, 2, 3], [1, 2, 3], [1, 2, 3]],
+            [1, 1]
+        )
+    };
+
+
+    [TestCaseSource(nameof(SmallestRange_SortedLists_ReturnsMinRangeCases))]
+    public void SmallestRange_SortedLists_ReturnsMinRange(TestData testData)
+    {
+        int[] result = new Solution().SmallestRange(testData.Lists);
+
+        result.Should().BeEquivalentTo(testData.Result);
+    }
+
+    public record TestData(IList<IList<int>> Lists, int[] Result);
+}
