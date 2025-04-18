39b9cad63d007c8084c593c9fbd300b1123e3c6b
Vladimir Demkin <lairotele@gmail.com>
2025-04-18 17:17:38 +0300

    Add solution for 416. Partition Equal Subset Sum

---
+++ LeetCode.Solutions/Medium/416. Partition Equal Subset Sum/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Solutions.Medium._416_Partition_Equal_Subset_Sum;
+
+/// <summary>
+///     https://leetcode.com/problems/partition-equal-subset-sum/
+/// </summary>
+public class Solution
+{
+    public bool CanPartition(int[] nums)
+    {
+        if (nums.Length <= 1) return false;
+
+        int sum = nums.Sum();
+        if (sum % 2 != 0) return false;
+        sum /= 2;
+
+        var possiblePartialSums = new HashSet<int> { 0 };
+        for (int i = 0; i <= nums.Length - 1; i++)
+        {
+            var newSums = new HashSet<int>();
+            foreach (int partialSum in possiblePartialSums)
+            {
+                int newSum = partialSum + nums[i];
+                if (newSum == sum)
+                    return true;
+                if (newSum < sum)
+                    newSums.Add(newSum);
+            }
+
+            possiblePartialSums.UnionWith(newSums);
+        }
+
+        return false;
+    }
+}
---
+++ LeetCode.Solutions/Medium/416. Partition Equal Subset Sum/Tests.cs
@@ -0,0 +1,20 @@
+using FluentAssertions;
+
+namespace LeetCode.Solutions.Medium._416_Partition_Equal_Subset_Sum;
+
+public class Tests
+{
+    [TestCase(new[] { 1, 5, 11, 5 })]
+    [TestCase(new[] { 3, 3, 3, 4, 5 })]
+    public void CanPartition_PartitionExists_ReturnsTrue(int[] nums)
+    {
+        new Solution().CanPartition(nums).Should().BeTrue();
+    }
+
+    [TestCase(new[] { 1, 2, 3, 5 })]
+    [TestCase(new[] { 2, 2, 3, 5 })]
+    public void CanPartition_PartitionDoesntExist_ReturnsFalse(int[] nums)
+    {
+        new Solution().CanPartition(nums).Should().BeFalse();
+    }
+}
