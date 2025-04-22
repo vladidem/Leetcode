6683402ba054bd046ec6e01453c9da45d8b04335
Vladimir Demkin <lairotele@gmail.com>
2025-04-22 20:38:29 +0300

    Fix naming, add case with no result

--- LeetCode.Solutions/Medium/494. Target Sum/Tests.cs
+++ LeetCode.Solutions/Medium/494. Target Sum/Tests.cs
@@ -6,7 +6,13 @@ public class Tests
     [TestCase(new[] { 1 }, 1, ExpectedResult = 1)]
     [TestCase(new[] { 22, 36, 7, 44, 38, 32, 16, 32, 1, 16, 25, 45, 49, 45, 27, 9, 41, 31, 10, 1 }, 1, ExpectedResult = 6060)]
     [Timeout(100)]
-    public int NumSubarrayProductLessThanK_ProductsExist_ReturnsNumOfSubarrays(int[] nums, int target)
+    public int FindTargetSumWays_SumExists_ReturnsNumOfWaysToSum(int[] nums, int target)
+    {
+        return new Solution().FindTargetSumWays(nums, target);
+    }
+
+    [TestCase(new[] { 1 }, 2, ExpectedResult = 0)]
+    public int FindTargetSumWays_SumDoesntExists_Returns0(int[] nums, int target)
     {
         return new Solution().FindTargetSumWays(nums, target);
     }
