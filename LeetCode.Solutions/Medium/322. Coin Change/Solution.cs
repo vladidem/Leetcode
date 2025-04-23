91ff61569a03b86d1e70faa0a476ff02121046a8
Vladimir Demkin <lairotele@gmail.com>
2025-04-23 12:57:00 +0300

    Add solution for 322. Coin Change

---
+++ LeetCode.Solutions/Medium/322. Coin Change/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode.Solutions.Medium._322_Coin_Change;
+
+/// <summary>
+///     https://leetcode.com/problems/coin-change/
+/// </summary>
+public class Solution
+{
+    private Dictionary<int, int> cache = new();
+
+    public int CoinChange(int[] coins, int amount)
+    {
+        cache = new Dictionary<int, int>();
+        return CoinChangeRecursive(coins, amount);
+    }
+
+    private int CoinChangeRecursive(int[] coins, int amount)
+    {
+        if (amount < 0) return -1;
+        if (amount == 0) return 0;
+        if (cache.TryGetValue(amount, out int cachedCoinsCount))
+            return cachedCoinsCount;
+
+        int coinsCount = -1;
+
+        for (int i = 0; i < coins.Length; i++)
+        {
+            int coinsCountWithCoinI = CoinChangeRecursive(coins, amount - coins[i]);
+            if (coinsCountWithCoinI == -1)
+                continue;
+
+            coinsCountWithCoinI++;
+
+            if (coinsCount == -1 || coinsCount > coinsCountWithCoinI)
+                coinsCount = coinsCountWithCoinI;
+        }
+
+        cache[amount] = coinsCount;
+
+        return coinsCount;
+    }
+}
---
+++ LeetCode.Solutions/Medium/322. Coin Change/Tests.cs
@@ -0,0 +1,18 @@
+namespace LeetCode.Solutions.Medium._322_Coin_Change;
+
+public class Tests
+{
+    [TestCase(new[] { 1, 2, 5 }, 11, ExpectedResult = 3)]
+    [Timeout(100)]
+    public int CoinChange_ChangeExists_ReturnsMinimumAmountOfCoins(int[] coins, int target)
+    {
+        return new Solution().CoinChange(coins, target);
+    }
+
+    [TestCase(new[] { 2 }, 3, ExpectedResult = -1)]
+    [Timeout(100)]
+    public int CoinChange_ChangeDoesntExist_ReturnsMinusOne(int[] coins, int target)
+    {
+        return new Solution().CoinChange(coins, target);
+    }
+}
