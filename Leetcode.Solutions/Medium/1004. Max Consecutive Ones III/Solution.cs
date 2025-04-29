963df7563561925e59c3d752776fb78767fdabd3
Vladimir Demkin <lairotele@gmail.com>
2025-04-29 20:42:52 +0300

    Add solution for 1004. Max Consecutive Ones III

---
+++ Leetcode.Solutions/Medium/1004. Max Consecutive Ones III/Solution.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Solutions.Medium._1004_Max_Consecutive_Ones_III;
+
+/// <summary>
+///     https://leetcode.com/problems/max-consecutive-ones-iii/
+/// </summary>
+public class Solution
+{
+    public int LongestOnes(int[] nums, int k)
+    {
+        if (nums.Length == 0) return 0;
+        if (nums.Length <= k) return k;
+
+        int windowStart = 0;
+        int windowEnd = 0;
+        int maxLength = k;
+        int windowZeroes = 0;
+        if (nums[0] == 0) windowZeroes = 1;
+
+        while (windowEnd < nums.Length - 1)
+        {
+            if (windowZeroes <= k)
+            {
+                windowEnd++;
+                if (nums[windowEnd] == 0) windowZeroes++;
+            }
+
+            if (windowZeroes > k)
+            {
+                if (nums[windowStart] == 0) windowZeroes--;
+                windowStart++;
+            }
+
+            if (windowZeroes <= k
+                && windowEnd - windowStart + 1 > maxLength)
+            {
+                maxLength = windowEnd - windowStart + 1;
+            }
+        }
+
+        return maxLength;
+    }
+}
---
+++ Leetcode.Solutions/Medium/1004. Max Consecutive Ones III/Tests.cs
@@ -0,0 +1,31 @@
+using FluentAssertions;
+
+namespace LeetCode.Solutions.Medium._1004_Max_Consecutive_Ones_III;
+
+public class Tests
+{
+    public static TestData[] LongestOnes_NotEmpty_ReturnsLengthOfMaxSequenceCases =
+    {
+        new(
+            [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0],
+            2,
+            6
+        ),
+        new(
+            [0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1],
+            3,
+            10
+        )
+    };
+
+
+    [TestCaseSource(nameof(LongestOnes_NotEmpty_ReturnsLengthOfMaxSequenceCases))]
+    public void LongestOnes_NotEmpty_ReturnsLengthOfMaxSequence(TestData testData)
+    {
+        int result = new Solution().LongestOnes(testData.Nums, testData.K);
+
+        result.Should().Be(testData.Expected);
+    }
+
+    public record TestData(int[] Nums, int K, int Expected);
+}
