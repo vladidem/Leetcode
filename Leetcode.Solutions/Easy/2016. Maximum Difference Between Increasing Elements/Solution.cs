500718423744ebf7aea9b1bcf17413434a3f914b
Vladimir Demkin <lairotele@gmail.com>
2025-06-16 17:34:08 +0300

    Add solution for 2016. Maximum Difference Between Increasing Elements

---
+++ Leetcode.Solutions/Easy/2016. Maximum Difference Between Increasing Elements/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace Leetcode.Solutions.Easy._2016._Maximum_Difference_Between_Increasing_Elements;
+
+/// <summary>
+///     https://leetcode.com/problems/maximum-difference-between-increasing-elements/
+/// </summary>
+public class Solution
+{
+    public int MaximumDifference(int[] nums)
+    {
+        if (nums.Length < 2) return 0;
+
+        int maxDiff = -1;
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[j] > nums[i]
+                    && nums[j] - nums[i] > maxDiff)
+                    maxDiff = nums[j] - nums[i];
+            }
+        }
+
+        return maxDiff;
+    }
+}
---
+++ Leetcode.Solutions/Easy/2016. Maximum Difference Between Increasing Elements/Tests.cs
@@ -0,0 +1,17 @@
+namespace Leetcode.Solutions.Easy._2016._Maximum_Difference_Between_Increasing_Elements;
+
+public class Tests
+{
+    [TestCase(new[] { 7, 1, 5, 4 }, ExpectedResult = 4)]
+    [TestCase(new[] { 1, 5, 2, 10 }, ExpectedResult = 9)]
+    public int MaximumDifference_DifferenceExists_ReturnsMaximumDifference(int[] nums)
+    {
+        return new Solution().MaximumDifference(nums);
+    }
+
+    [TestCase(new[] { 9, 4, 3, 2 }, ExpectedResult = -1)]
+    public int MaximumDifference_DifferenceDoesntExist_ReturnsMinusOne(int[] nums)
+    {
+        return new Solution().MaximumDifference(nums);
+    }
+}
