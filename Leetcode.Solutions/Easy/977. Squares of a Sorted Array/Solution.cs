4e5ef4a709826c441736927b8342145a6b6b751e
Vladimir Demkin <lairotele@gmail.com>
2025-06-18 12:18:15 +0300

    Alter naming

--- Leetcode.Solutions/Easy/977. Squares of a Sorted Array/Solution.cs
+++ Leetcode.Solutions/Easy/977. Squares of a Sorted Array/Solution.cs
@@ -7,36 +7,59 @@ public class Solution
 {
     public int[] SortedSquares(int[] nums)
     {
-        int firstNonNegative = nums.Length - 1;
-
-        for (int i = 0; i < nums.Length - 1; i++)
-        {
-            if (nums[i] >= 0)
-            {
-                firstNonNegative = i;
-                break;
-            }
-        }
-
-        int left = Math.Max(firstNonNegative - 1, 0);
-        int right = left + 1;
         int[] result = new int[nums.Length];
+        int i = 0;
+        (int negativePointer, int positivePointer) = GetMiddle(nums);
 
-        for (int i = 0; i <= nums.Length - 1; i++)
+        while (negativePointer >= 0 || positivePointer <= nums.Length - 1)
         {
-            if (right > nums.Length - 1
-                || (left >= 0 && (int) Math.Pow(nums[left], 2) <= (int) Math.Pow(nums[right], 2)))
+            if (negativePointer < 0
+                || (positivePointer <= nums.Length - 1
+                    && Math.Abs(nums[positivePointer]) < Math.Abs(nums[negativePointer])))
             {
-                result[i] = (int) Math.Pow(nums[left], 2);
-                left--;
+                result[i] = nums[positivePointer] * nums[positivePointer];
+                i++;
+                positivePointer++;
             }
             else
             {
-                result[i] = (int) Math.Pow(nums[right], 2);
-                right++;
+                result[i] = nums[negativePointer] * nums[negativePointer];
+                i++;
+                negativePointer--;
             }
         }
 
         return result;
+    }
+
+    private (int negativePointer, int positivePointer) GetMiddle(int[] nums)
+    {
+        int negativePointer = 0;
+        int positivePointer = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i == 0 && nums[i] >= 0)
+            {
+                negativePointer = -1;
+                positivePointer = 0;
+                break;
+            }
+
+            if (i == nums.Length - 1 && nums[i] <= 0)
+            {
+                negativePointer = nums.Length - 1;
+                positivePointer = nums.Length;
+                break;
+            }
+
+            if (i > 0 && nums[i - 1] <= 0 && nums[i] >= 0)
+            {
+                negativePointer = i - 1;
+                positivePointer = i;
+            }
+        }
+
+        return (negativePointer, positivePointer);
     }
 }
