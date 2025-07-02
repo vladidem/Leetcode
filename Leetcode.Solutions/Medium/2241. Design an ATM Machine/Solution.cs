33340ce06fca1bae5ea953de28b56519ac765aeb
Vladimir Demkin <lairotele@gmail.com>
2025-07-02 19:42:37 +0300

    Add solution for 2241. Design an ATM Machine

---
+++ Leetcode.Solutions/Medium/2241. Design an ATM Machine/Solution.cs
@@ -0,0 +1,62 @@
+﻿namespace Leetcode.Solutions.Medium._2241._Design_an_ATM_Machine;
+
+/// <summary>
+///     https://leetcode.com/problems/design-an-atm-machine/
+/// </summary>
+public class ATM
+{
+
+    private readonly int[] _banknotesCounts =
+    [
+        0,
+        0,
+        0,
+        0,
+        0
+    ];
+
+    private readonly int[] _banknotesValue =
+    [
+        20,
+        50,
+        100,
+        200,
+        500
+    ];
+
+    public void Deposit(int[] banknotesCount)
+    {
+        for (int banknote = 0; banknote < banknotesCount.Length; banknote++)
+        {
+            _banknotesCounts[banknote] += banknotesCount[banknote];
+        }
+    }
+
+    public int[] Withdraw(int amount)
+    {
+        int[] banknotesToDraw = Enumerable.Range(0, 5).Select(_ => 0).ToArray();
+        for (int banknote = _banknotesCounts.Length - 1; banknote >= 0 && amount > 0; banknote--)
+        {
+            if (amount >= _banknotesValue[banknote]
+                && _banknotesCounts[banknote] > banknotesToDraw[banknote]
+               )
+            {
+                int toDraw = Math.Min(_banknotesCounts[banknote], amount / _banknotesValue[banknote]);
+                amount -= _banknotesValue[banknote] * toDraw;
+                banknotesToDraw[banknote] += toDraw;
+            }
+        }
+
+        if (amount == 0)
+        {
+            for (int banknote = 0; banknote < banknotesToDraw.Length; banknote++)
+            {
+                _banknotesCounts[banknote] -= banknotesToDraw[banknote];
+            }
+
+            return banknotesToDraw;
+        }
+
+        return [-1];
+    }
+}
